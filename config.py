b069b482c42a0a7d50762617364a38b8568339e4
Tim Perry <pimterry@gmail.com>
2014-02-24 02:33:17 +0000

    Initial commit with default config

---
+++ config.py
@@ -0,0 +1,110 @@
+from libqtile.config import Key, Screen, Group
+from libqtile.command import lazy
+from libqtile import layout, bar, widget
+
+mod = "mod4"
+
+keys = [
+    # Switch between windows in current stack pane
+    Key(
+        [mod], "k",
+        lazy.layout.down()
+    ),
+    Key(
+        [mod], "j",
+        lazy.layout.up()
+    ),
+
+    # Move windows up or down in current stack
+    Key(
+        [mod, "control"], "k",
+        lazy.layout.shuffle_down()
+    ),
+    Key(
+        [mod, "control"], "j",
+        lazy.layout.shuffle_up()
+    ),
+
+    # Switch window focus to other pane(s) of stack
+    Key(
+        [mod], "space",
+        lazy.layout.next()
+    ),
+
+    # Swap panes of split stack
+    Key(
+        [mod, "shift"], "space",
+        lazy.layout.rotate()
+    ),
+
+    # Toggle between split and unsplit sides of stack.
+    # Split = all windows displayed
+    # Unsplit = 1 window displayed, like Max layout, but still with
+    # multiple stack panes
+    Key(
+        [mod, "shift"], "Return",
+        lazy.layout.toggle_split()
+    ),
+    Key([mod], "Return", lazy.spawn("xterm")),
+
+    # Toggle between different layouts as defined below
+    Key([mod], "Tab",    lazy.nextlayout()),
+    Key([mod], "w",      lazy.window.kill()),
+
+    Key([mod, "control"], "r", lazy.restart()),
+    Key([mod], "r", lazy.spawncmd()),
+]
+
+groups = [
+    Group("a"),
+    Group("s"),
+    Group("d"),
+    Group("f"),
+    Group("u"),
+    Group("i"),
+    Group("o"),
+    Group("p"),
+]
+for i in groups:
+    # mod1 + letter of group = switch to group
+    keys.append(
+        Key([mod], i.name, lazy.group[i.name].toscreen())
+    )
+
+    # mod1 + shift + letter of group = switch to & move focused window to group
+    keys.append(
+        Key([mod, "shift"], i.name, lazy.window.togroup(i.name))
+    )
+
+dgroups_key_binder = None
+dgroups_app_rules = []
+
+layouts = [
+    layout.Max(),
+    layout.Stack(stacks=2)
+]
+
+screens = [
+    Screen(
+        bottom=bar.Bar(
+            [
+                widget.GroupBox(),
+                widget.Prompt(),
+                widget.WindowName(),
+                widget.TextBox("default config", name="default"),
+                widget.Systray(),
+                widget.Clock('%Y-%m-%d %a %I:%M %p'),
+            ],
+            30,
+        ),
+    ),
+]
+
+main = None
+follow_mouse_focus = True
+bring_front_click = False
+cursor_warp = False
+floating_layout = layout.Floating()
+mouse = ()
+auto_fullscreen = True
+widget_defaults = {}
