bb662c3790a2797b3c3f08697366b04bf8b94e5b
Tim Perry <pimterry@gmail.com>
2014-02-25 23:18:30 +0000

    Move to a proper tiled layout + shortcuts

--- config.py
+++ config.py
@@ -39,51 +39,12 @@ keys = [
     Key([mod], "q", lazy.window.kill()),
 
 
-    # Switch between windows in current stack pane
-    Key(
-        [mod], "k",
-        lazy.layout.down()
-    ),
-    Key(
-        [mod], "j",
-        lazy.layout.up()
-    ),
-
-    # Move windows up or down in current stack
-    Key(
-        [mod, "control"], "k",
-        lazy.layout.shuffle_down()
-    ),
-    Key(
-        [mod, "control"], "j",
-        lazy.layout.shuffle_up()
-    ),
-
-    # Switch window focus to other pane(s) of stack
-    Key(
-        [mod], "space",
-        lazy.layout.next()
-    ),
-
-    # Swap panes of split stack
-    Key(
-        [mod, "shift"], "space",
-        lazy.layout.rotate()
-    ),
-
-    # Toggle between split and unsplit sides of stack.
-    # Split = all windows displayed
-    # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
-    Key(
-        [mod, "shift"], "Return",
-        lazy.layout.toggle_split()
-    ),
-    Key([mod], "Return", lazy.spawn("xterm")),
-
-    # Toggle between different layouts as defined below
-    Key([mod], "Tab",    lazy.nextlayout()),
-    Key([mod], "w",      lazy.window.kill()),
+    Key([mod], "j", lazy.layout.down()),
+    Key([mod], "k", lazy.layout.up()),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down()),
+    Key([mod, "shift"], "k", lazy.layout.shuffle_up()),
+    
+    Key([mod], "m",    lazy.nextlayout()),
 
     Key([mod, "control"], "r", lazy.restart()),
 ]
@@ -114,7 +75,7 @@ dgroups_app_rules = []
 
 layouts = [
     layout.Max(),
-    layout.Stack(stacks=2)
+    layout.MonadTall()
 ]
 
 screens = [
