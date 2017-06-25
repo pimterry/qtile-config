9a1b3fc88dc12f90b2fcbf470b8b42e2fd27b08c
Tim Perry <pimterry@gmail.com>
2017-06-25 23:53:22 +0200

    Add screens

--- config.py
+++ config.py
@@ -43,7 +43,11 @@ groups = [
     Group("1"),
     Group("2"),
     Group("3"),
-    Group("4")
+    Group("4"),
+    Group("5"),
+    Group("6"),
+    Group("7"),
+    Group("8")
 ]
 
 dgroups_key_binder = None
